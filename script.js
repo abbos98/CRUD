e939898a4fa8161be10d9a5a37b7bfe0e3a58446
Abbos <abbos.pankiy2018@gmail.com>
2024-03-19 17:01:39 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+
+// CRUD => CREATE, READ, UPDATE, DELETE
+
+// let cars = [
+//     { id: 1, year: 1998, engine: 1, name: 'Tico'},
+//     { id: 2, year: 2005, engine: 1.2, name: 'Matiz'},
+//     { id: 3, year: 2010, engine: 1.6, name: 'Gentra'},
+//     { id: 4, year: 2010, engine: 1.5, name: 'Cobalt'},
+//     { id: 5, year: 2012, engine: 2, name: 'Malibu'},
+//     { id: 6, year: 2000, engine: 1.2, name: 'Damas'},
+//     { id: 7, year: 2018, engine: 2.4, name: 'Tracker'}
+// ];
+
+// const onCreate = (car) => {
+//  cars = {id:cars.length + 1, ...car}
+// }
+// onCreate({year:2020,engine: 1.3, name: 'A New car'});
+// console.log(cars);
+
+// function Cars() {
+//     return cars;
+// }
+// console.log(cars)
+
+
+
+
+
+
+
+
+
+// const onDelete = (id) => {
+    
+// }
+
+// onDelete(7);
+
+// ==========================
+
+// const onUpdate = (car) => {
+    
+// }
+
+// onUpdate({});
+
+// ==========================
+
+// const onRead = () => {
+    
+// }
+
+// onRead();
+
+// ==========================
+
+// const onCreate = (car) => {
+    
+// }
+
+// onCreate({});
+
